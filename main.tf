261a07cc983058f9160c25db5c9849933d961dc6
FehGihCloud <felps.sousa.franca@gmail.com>
2026-03-09 12:04:50 -0300

    Add S3 bucket and website configuration for frontend

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Bucket S3 para frontend
+resource "aws_s3_bucket" "frontend" {
+  bucket = "felipe-terraform-frontend-test"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_site" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
